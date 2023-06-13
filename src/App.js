2d0c80b87805586791a801dde9f88d36c2021a38
tau chipurura <tauraichipurura@gmail.com>
2023-06-13 17:48:59 +0200

    redux second project posts blog

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import Counter from './features/counter/Counter';
+import PostLists from './features/posts/PostLists';
+import AddPostForm from './features/posts/AddPostForm'
+
 
 function App() {
   return (
-    <div className="App">
-      <Counter/>
+    <main className="App">
+      <AddPostForm/>
+      <PostLists/>
       
-    </div>
+     
+      
+    </main>
   );
 }
 
